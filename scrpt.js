da5086f24cdccf04d00c9d48cbe86b549aaf9007
Deepak <deepakkumar09092003@gmail.com>
2024-04-25 13:51:05 +0530

    msg

--- scrpt.js
+++ scrpt.js
@@ -1,5 +1,7 @@
 const loading = document.querySelector('.loading');
 const submitBtn = document.querySelector('#submitBtn');
+const inputField = document.querySelector('#input-large');
+
 submitBtn.addEventListener('click', () => {
     loading.style.display = 'flex';
     submitBtn.style.cursor = 'not-allowed';
@@ -9,10 +11,11 @@ submitBtn.addEventListener('click', () => {
         loading.style.display = 'none';
         submitBtn.style.textDecoration = 'none'
         submitBtn.style.cursor = 'pointer';
+        inputField.value='';
+
     }, 2000);
 })
 
-const inputField = document.querySelector('.input-large');
 inputField.addEventListener('input', function () {
     inputField.classList.add('typing');
 });
